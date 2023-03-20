a88de86f4d831afe63267d8bc738f466241f986c
Tosin Adejuwon <tosin0601@gmail.com>
2023-03-20 13:25:29 -0700

    lastlast

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 /**
- * main -UNIX
- * Return: Always 1 (Success)
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-const char message[];
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-message [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, sizeof(message) - 1);
+	write(STDERR_FILENO, message, sizeof(message) - 1);
 return (1);
 }
