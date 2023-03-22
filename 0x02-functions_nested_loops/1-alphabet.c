8993493de889634d3cad3d691ea075b7b187fe25
Tosin Adejuwon <tosin0601@gmail.com>
2023-03-22 07:18:04 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main- print alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	_putchar ('abcdefghijklmnopqrstuvwxzy');
+	_putchar ('\n');
+	return (0);
+}
